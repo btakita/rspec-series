659c7c618cb42e6b89298a3c558f3198612ac8db
Brian Takita <brian.takita@gmail.com>
2009-08-04 00:00:14 -0700

    Renamed rspec/series to spec/series.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require "spec"
 require "spec/autorun"
 require "rr"
 $:.unshift(File.expand_path("#{File.dirname(__FILE__)}/../lib"))
-require "rspec/series"
+require "spec/series"
 ARGV.push("-b", "-fn")
 
 Spec::Runner.configure do |config|

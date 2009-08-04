1bd154f7b538a8a54f1f696b24622db1e79526f7
Brian Takita <brian.takita@gmail.com>
2009-08-04 00:07:38 -0700

    Removed puts messages.

--- lib/spec/series/example_group_methods.rb
+++ lib/spec/series/example_group_methods.rb
@@ -1,11 +1,6 @@
 module Spec
   module Series
     module ExampleGroupMethods
-      def self.included(mod)
-        p mod
-        puts "#{__FILE__}:#{__LINE__}"
-      end
-
       def series(*args, &block)
         it(*args, &block)
       end
